29a073221c766b587ac3b1571b42f5d35707a3e1
Vikram Sinha <micky@Vikrams-MacBook-Pro.local>
2024-03-15 23:03:30 +0530

    initial commit - Accordion: version 01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './index.css';
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text:
+      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text:
+      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion data={faqs}/>
     </div>
-  );
+  )
+}
+
+function Accordion({data}) {
+  return (
+  <div className="accordion">
+    {data.map((item, i) => (
+    <AccordionItems title={item.title} text={item.text} num={i} key={item.title}/>
+  ))}
+  </div>
+  )
+}
+
+function AccordionItems({title, text, num}) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  function handleClick() {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <div className={`item ${isOpen ? 'open' : ''}`} onClick={handleClick}>
+      <p className="number">{num < 9 ? `0${num+1}` : num+1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? '-' : '+'}</p>
+      {isOpen && <p className="content-box">{text}</p>}
+    </div>
+  )
 }
 
 export default App;
